419f5b7ddae1d86e7df28311b998730c8f68008c
curvesy <noobesang@gmail.com>
2025-12-31 07:53:12 +0330

    fix: Add peft/__init__.py for proper module imports

---
+++ stage1_ultimate/src/peft/__init__.py
@@ -0,0 +1,16 @@
+"""PEFT (Parameter-Efficient Fine-Tuning) modules"""
+
+from peft.explora_domain import (
+    SimCLRLoss,
+    create_projection_head,
+    create_explora_config,
+    apply_explora_to_backbone,
+)
+
+__all__ = [
+    "SimCLRLoss",
+    "create_projection_head",
+    "create_explora_config",
+    "apply_explora_to_backbone",
+]
+
